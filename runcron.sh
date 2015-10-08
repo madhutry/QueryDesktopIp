f0c965c69333d7d2cdd895b8e524e91166f95e81
MAdhukar <madhukar.tryambake@gmail.com>
2015-10-08 19:31:14 +0000

    some

--- runcron.sh
+++ runcron.sh
@@ -1,2 +1,3 @@
+cd /home/pi/sendip/
 /home/pi/sendip/collectip.sh >> /home/pi/sendip/sendip.txt
 ssh-agent bash -c 'ssh-add  /home/pi/.ssh/githubip_rsa; git fetch; git pull; git commit -a -m "some"; git push '
