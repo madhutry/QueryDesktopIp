39c9ef02bf546f54ae8db23fc84b7152f24f73c4
MAdhukar <madhukar.tryambake@gmail.com>
2015-10-12 09:02:06 +0000

    some

--- collectip.sh
+++ collectip.sh
@@ -1,5 +1,6 @@
 curl http://ipecho.net/plain
 wget http://ipecho.net/plain -O /home/pi/sendip/sendip.txt
 wget http://ipecho.net/plain -O /home/pi/sendip/sendip1.txt
+echo "::::" >>  /home/pi/sendip/sendip.txt
 echo `date` >>  /home/pi/sendip/sendip.txt
 
--- runcrondiff.sh
+++ runcrondiff.sh
@@ -4,7 +4,7 @@ DIFF=$(diff t1.txt t2.txt)
 if [ "$DIFF" != "" ]
 then
     echo "The directory was modified"
-    /home/pi/sendip/collectip.sh >> /home/pi/sendip/sendip.txt
+    /home/pi/sendip/collectip.sh
     ssh-agent bash -c 'ssh-add  /home/pi/.ssh/githubip_rsa; git fetch; git pull; git commit -a -m "some"; git push '
     cp -rf t1.txt t2.txt
 fi
