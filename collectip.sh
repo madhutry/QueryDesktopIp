09a99ec3d136c8323d9febbf1931a6716a1269aa
MAdhukar <madhukar.tryambake@gmail.com>
2015-10-12 15:25:00 +0000

    se

--- collectip.sh
+++ collectip.sh
@@ -1,6 +1,9 @@
 curl http://ipecho.net/plain
-wget http://ipecho.net/plain -O /home/pi/sendip/sendip.txt
-wget http://ipecho.net/plain -O /home/pi/sendip/sendip1.txt
+wget http://ipecho.net/plain -a -O /home/pi/sendip/sendip.txt
+echo "::::" >>  /home/pi/sendip/sendip.txt
+wget http://ipecho.net/plain -a -O /home/pi/sendip/sendip.txt
+echo "::::" >>  /home/pi/sendip/sendip.txt
+wget http://ipecho.net/plain -a -O /home/pi/sendip/sendip1.txt
 echo "::::" >>  /home/pi/sendip/sendip.txt
 echo `date` >>  /home/pi/sendip/sendip.txt
 
