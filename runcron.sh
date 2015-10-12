b829c8e7921fd61c574b94c3b44fd6c8502f0c9f
madhutry <madhukar.tryambake@gmail.com>
2015-10-12 12:15:16 +0530

    Update runcron.sh

--- runcron.sh
+++ runcron.sh
@@ -1,4 +1,5 @@
 cd /home/pi/sendip/
 ssh-agent bash -c 'ssh-add  /home/pi/.ssh/githubip_rsa; git fetch; git pull;'
+wget http://ipecho.net/plain -O /home/pi/sendip/sendip.txt
 /home/pi/sendip/collectip.sh >> /home/pi/sendip/sendip.txt
 ssh-agent bash -c 'ssh-add  /home/pi/.ssh/githubip_rsa; git fetch; git pull; git commit -a -m "some"; git push '
