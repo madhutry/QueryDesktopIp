af21409af7cf63b61831aa4a9aa2fa8500fe0011
madhukar.tryambake <madhukar.tryambake@gmail.com>
2015-10-24 08:47:56 +0000

    some

--- runcrondiff.sh
+++ runcrondiff.sh
@@ -1,10 +1,10 @@
-cd /home/pi/sendip/
-ssh-agent bash -c 'ssh-add  /home/pi/.ssh/githubip_rsa; git fetch; git pull;'
+cd ~/github/testrep
+ssh-agent bash -c 'ssh-add  ~/.ssh/githubnopass_rsa; git fetch; git pull;'
 DIFF=$(diff t1.txt t2.txt)
 if [ "$DIFF" != "" ]
 then
     echo "The directory was modified"
-    /home/pi/sendip/collectip.sh
-    ssh-agent bash -c 'ssh-add  /home/pi/.ssh/githubip_rsa; git fetch; git pull; git commit -a -m "some"; git push '
+    ./collectip.sh
+    ssh-agent bash -c 'ssh-add  ~/.ssh/githubnopass_rsa; git fetch; git pull; git commit -a -m "some"; git push '
     cp -rf t1.txt t2.txt
 fi
